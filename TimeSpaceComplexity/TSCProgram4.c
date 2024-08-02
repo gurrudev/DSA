1f2048d826071d458ac0fbc2a6502fe9db4a3e23
Ashutosh Pawar <pawarash000@outlook.com>
2024-08-02 14:12:53 +0530

    updated

--- TimeSpaceComplexity/TSCProgram4.c
+++ TimeSpaceComplexity/TSCProgram4.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 
 
-int findDuplicate(int arr[], int size){
+int findDuplicateElement(int arr[], int size){
 
     // Solution #1: Nested Loop
 
@@ -24,33 +24,69 @@ int findDuplicate(int arr[], int size){
             }
 
         }
+        // if(flag == 0) break;
     }
     */
 
     // Solution #2: Temperory Array
-    
+
+    /*
+    int temp[size];
+    for(int i = 0; i <= size; i++){
+        if(temp[arr[i]] == 1){
+            printf("%d", arr[i]);
+            break;
+        }
+        temp[arr[i]] = 1;
+    }
+    */
+
+    // Solution #3: 
+
+    int sum = 0;
+    int result = 0; 
+
+    for(int i = 0; i <= size; i++){
+        sum += arr[i];
+    }
+
+    // Because we have size of an array is n+1 and 1 element is repeted and we have to reduce it.
+    int s = (size * (size+1)) / 2;
+
+    result = sum - s;
+
+    printf("%d", result);
 
 }
 
 int main(){
-    int arr[] = {5, 1, 4, 3, 2, 3};
+    int arr[] = {5, 1, 4, 4, 2, 3};
     int size = sizeof(arr)/sizeof(arr[0]);
-    findDuplicate(arr, size);
+    findDuplicateElement(arr, size);
     return 0;
 }
 
 /*  
+    Conclusion: 
+
     Solution #1: Nested Loop
-    Conclusion: Time Complexity 
 
-    => k1n + k2n
-    =  (k1 + k2)n
-    =  k3n -> O(n) 
-
-    +---------+
-    +   O(n)  +
-    +---------+
+    => k1n + k2n + 1
+    =  (k1 + k2)n + 1
+    =  k3n + 1 -> O(n+1) 
+    => Time Coplexity: O(n+1)
+    => Space Complexity: O(1)
+    
 
     Solution #2: Temperory Array
+
+    => Time Coplexity: O(n)
+    => Space Complexity: O(n+n)
+
+
+    // Solution #3:
+
+    => Time Coplexity: O(n)
+    => Space Complexity: O(1)
 
 */
