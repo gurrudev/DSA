64480288ff42fa1b6d8a7ce230f516305e383bec
Ashutosh Pawar <pawarash000@outlook.com>
2024-08-18 11:07:40 +0530

    Bubble Sort Updated

--- Sorting/BubbleSort.c
+++ Sorting/BubbleSort.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int bubbleSort(int arr[], int size){
-    for(int i = 0; i < size; i++){
-        for(int j = 0; j < size - i; j++){
+    for(int i = 0; i < size-1; i++){
+        for(int j = 0; j < size-1 - i; j++){
             if(arr[j] > arr[j+1]){
                 int temp = arr[j];
                 arr[j] = arr[j+1];
