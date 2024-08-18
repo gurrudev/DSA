d2f1e660d3b87d4ebcf416f204ae6b75991de834
Ashutosh Pawar <pawarash000@outlook.com>
2024-08-18 09:47:39 +0530

    Binary Search Added

--- Searching/BinarySearch.c
+++ Searching/BinarySearch.c
@@ -9,7 +9,7 @@ int binarySearch(int arr[], int size, int element){
         
         if(arr[mid] == element){
             return mid;
-        } else if (arr[mid] > element){
+        } else if (arr[mid] < element){
             start = mid + 1;
         } else {
             end = mid - 1;
@@ -20,12 +20,12 @@ int binarySearch(int arr[], int size, int element){
 }
 
 int main(){
-    // int arr[] = {11, 12, 54, 65, 76, 98};    // Ascending  (arr[mid] < element)
-    int arr[] = {98, 76, 65, 54, 12, 11};    // Descending  (arr[mid] > element)
+    int arr[] = {11, 12, 54, 65, 76, 98};    // Ascending  (arr[mid] < element)
+    // int arr[] = {98, 76, 65, 54, 12, 11};    // Descending  (arr[mid] > element)
 
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    int result = binarySearch(arr, size, 11);
+    int result = binarySearch(arr, size, 65);
     printf("%d", result);
 
     return 0; 
